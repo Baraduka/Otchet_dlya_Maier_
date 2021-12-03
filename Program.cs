684c743066d67687b1dfea6056d1b34ec67b4838
Baraduka <sitkin2@yandex.ru>
2021-12-03 20:27:57 +0500

    Otchet_dlya_Maier_

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Лаб.раб._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s, r; int i;
+            Console.WriteLine("Введите s:");
+            s = Convert.ToString(Console.ReadLine());
+            string[] A = s.Split(' ');
+            for (i = 0; i < A.Length; i++)
+                if (A[i].EndsWith("я"))
+                    A[i] = A[i] = A[i].Remove(0, 3);
+            r = string.Join(" ", A);
+            Console.WriteLine(r);
+            Console.ReadKey();
+        }
+    }
+}
